3bee8e40c7fcd13dd334fa5c31810cf0627e9cb9
NaobePorras <naobe_1321124029@uptecamac.edu.mx>
2021-09-15 14:24:10 -0500

    se agregan practicas

---
+++ practicas/test.js
@@ -0,0 +1,15 @@
+var n1 = 9
+var n2 = 3 
+var n3 = 20
+var n4 = 4 
+var n5 = 18
+
+if (n1 > n5) {
+    console.log("n1 es mayor que n5");
+}else{
+    console.log("n1 es menor que n5")
+}
+
+if (n5 <= n3) {
+    
+}
