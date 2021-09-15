2db1d24f56be9915770d6a53382de73dd236c8b0
NaobePorras <naobe_1321124029@uptecamac.edu.mx>
2021-09-15 15:17:10 -0500

    subir test.js

--- practicas/test.js
+++ practicas/test.js
@@ -1,15 +1,35 @@
-var n1 = 9
-var n2 = 3 
-var n3 = 20
-var n4 = 4 
-var n5 = 18
+var n1 = 9990
+var n2 = 3890
+var n3 = 2000
+var n4 = 4090
+var n5 = 1878
 
 if (n1 > n5) {
     console.log("n1 es mayor que n5");
 }else{
-    console.log("n1 es menor que n5")
+    console.log("n1 es menor que n5");
 }
 
 if (n5 <= n3) {
-    
+    console.log("n5 es menor o igual que n3");
+}else{
+    console.log("n5 es mayor o igual que n3");
+}
+
+if (n4 > n2) {
+     console.log("n4 es mayor que n2");
+}else{
+    console.log("n4 es menor que n2");
+}
+
+if (n3 < n2) {
+    console.log("n3 es menor que n2");
+}else{
+    console.log("n3 es mayor que n2");
+}
+
+if (n2 > n1 ) {
+    console.log("n2 es mayor que n1");
+}else{
+    console.log("n2 es menor que n1");
 }
