8e970a382a850941a80f5b1e7a91ca61664a0678
NaobePorras <naobe_1321124029@uptecamac.edu.mx>
2021-09-27 14:32:14 -0500

    arreglos

---
+++ practicas/arreglos asociativos/arreglos asociativos.js
@@ -0,0 +1,26 @@
+// -- Arreglo Asociativo
+var elementos3 = {
+    "nombre": "Naobe",
+    "edad" : "18",
+    "casada" : false,
+    "especialidad" : {
+        "fronted" :{
+          "html" : false,
+          "css" : false,
+          "js" : false,
+        },
+        "backend": {
+           "php": false,
+           "java" : false,
+           "c#" : false,
+        },
+        "mobile" : {
+            "android" : false,
+            "ios" : false,
+            "windows": false
+        },
+
+    },
+
+}
+console.log(elementos3);
