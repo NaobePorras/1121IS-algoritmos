bd9a8e508a30407be43c0af0d480b40c03b8fcb6
NaobePorras <naobe_1321124029@uptecamac.edu.mx>
2021-09-27 17:38:49 -0500

    guardar

---
+++ practicas/arreglos asociativos/tarea con arreglos.js
@@ -0,0 +1,37 @@
+var datos = {
+    "nombre": "Naobe",
+    "edad" : 18,
+    "color favorito" : "verde agua",
+    "casada" : false,
+    "comida" : {
+        "frutas" :{
+          "manzana" : true,
+          "platano" : true,
+          "uvas" : true,
+        },
+        "potres": {
+           "gelatina": false,
+           "flan" : true,
+           "lechera" : false,
+        },
+        "agua" : {
+            "horchata" : "nunca",
+            "jamaica" : "siempre",
+            "limon": "siempre",
+        },
+
+    },
+    "musica" : {
+        "rock" :{
+            "AC/DC" : true,
+            "Queen" : true,
+            "linkin park": true,
+        },
+         "pop" :{
+            "Louane" : true,
+            "Bruno mars": true,
+            "maroon 5": true,
+         }
+    }
+}
+console.log(datos);
